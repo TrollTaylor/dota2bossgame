ea8d21afa0efe8c4bca18581e1e37163a82fc940
Andypro1213 <andyrts2003@yahoo.com>
2015-09-15 19:01:27 -0400

    here u go jew

--- game/dota_addons/bossgamegit/scripts/vscripts/addon_game_mode.lua
+++ game/dota_addons/bossgamegit/scripts/vscripts/addon_game_mode.lua
@@ -132,7 +132,7 @@ end
 
 
 function BossGame:InitGameMode()
-	print("The poop has dropped!!!")
+	print("fuk the police coming straight from the underground a young andy got it back cause hes brown and all the other colors so police think they have the authori")
 	GameRules:GetGameModeEntity():SetThink( "OnThink", self, "GlobalThink", 2 )
 	GameRules:GetGameModeEntity():SetAnnouncerDisabled(true)
 	GameRules:GetGameModeEntity():SetLoseGoldOnDeath(false)
