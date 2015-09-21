6b0ce20c6ee67bf3de66de3fb3e9a6b5ac7fc3db
Andypro1213 <andyrts2003@yahoo.com>
2015-09-20 23:07:16 -0400

    ju

--- game/dota_addons/bossgamegit/scripts/vscripts/ranger_abilities.lua
+++ game/dota_addons/bossgamegit/scripts/vscripts/ranger_abilities.lua
@@ -25,4 +25,30 @@ function arrow_shoot(event)
 	}	
 	ProjectileManager:CreateLinearProjectile(info)
 
+
+		local info2 = {
+		Ability = event.ability,
+		EffectName = "particles/units/heroes/hero_mirana/mirana_spell_arrow.vpcf",
+		vSpawnOrigin = event.caster:GetAbsOrigin(),
+ 	fDistance = 2000,
+ 	fStartRadius = 64,
+ 	fEndRadius = 64,
+ 	Source = event.caster,
+ 	bHasFrontalCone = false,
+ 	bReplaceExisting = false,
+ 	iUnitTargetTeam = DOTA_UNIT_TARGET_TEAM_ENEMY,
+ 	iUnitTargetFlags = DOTA_UNIT_TARGET_FLAG_NONE,
+ 	iUnitTargetType = DOTA_UNIT_TARGET_HERO,
+ 	fExpireTime = GameRules: GetGameTime() + 10.0,
+ 	bDeleteOnHit = true,
+ 	vVelocity = event.caster:GetAnglesAsVector() * 1800,
+ 	bProvidesVision = true,
+ 	iVisionRadius = 1000,
+ 	iVisionTeamNumber = event.caster:GetTeamNumber(),
+ 	fMaxSpeed = 20
+
+
+
+	}	
+	ProjectileManager:CreateLinearProjectile(info2)
 	end	
